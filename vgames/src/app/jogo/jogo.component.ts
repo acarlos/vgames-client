3903b51aa9300435b6e05c03aa2f25d888a95031
acarlos <acarlos.mendesjr@gmail.com>
2022-06-20 16:54:06 -0300

    Gravando respostas

--- vgames/src/app/jogo/jogo.component.ts
+++ vgames/src/app/jogo/jogo.component.ts
@@ -4,13 +4,15 @@ import { RespostaDTO } from '../models/resposta-dto.model';
 import { SherlockDTO } from '../models/shelock-dto.model';
 import { JogoService } from '../services/jogo.service';
 
+declare var webkitSpeechRecognition: any;
+
 @Component({
   selector: 'app-jogo',
   templateUrl: './jogo.component.html',
   styleUrls: ['./jogo.component.css']
 })
 export class JogoComponent implements OnInit {
-  constructor(private jogoService: JogoService){}
+  constructor(private jogoService: JogoService) { }
 
   title = 'vgames';
   sherlockDTO: SherlockDTO = new SherlockDTO();
@@ -18,16 +20,16 @@ export class JogoComponent implements OnInit {
   public respostaVazia: boolean = true;
   public jogoIniciado: boolean = false;
 
-  getStartGame(){
+  getStartGame() {
     this.jogoService.getStartGame().subscribe(data => {
-      this.sherlockDTO=data;
+      this.sherlockDTO = data;
       console.log(this.sherlockDTO);
     });
   }
 
   pergunta() {
-    this.jogoService.getPergunta(this.sherlockDTO.indice +1).subscribe(data => {
-      this.sherlockDTO=data;
+    this.jogoService.getPergunta(this.sherlockDTO.indice + 1).subscribe(data => {
+      this.sherlockDTO = data;
       let audioElement = <HTMLAudioElement>document.getElementById('perguntaVoz');
       audioElement.src = data.perguntaDTO.perguntaVoz;
       audioElement.play();
@@ -35,9 +37,10 @@ export class JogoComponent implements OnInit {
   }
 
   responda() {
-    this.sherlockDTO = new SherlockDTO();
+    var textInVoice = <HTMLInputElement>document.getElementById("resposta");
+    this.resposta = textInVoice.value;
     this.jogoService.setResposta(this.resposta).subscribe(data => {
-      this.sherlockDTO=data;
+      this.sherlockDTO = data;
       this.resposta = "";
       this.respostaVazia = true;
     });
@@ -65,5 +68,34 @@ export class JogoComponent implements OnInit {
 
   ngOnInit(): void {
     this.getStartGame();
+  }
+
+  recognition = new webkitSpeechRecognition();
+
+  iniciarAudicaoPergunta() {
+    var textInVoice = <HTMLInputElement>document.getElementById("resposta");
+    this.recognition.continuous = true;
+    this.recognition.start();
+    this.recognition.onend = function (event) {
+      if (textInVoice.value == "") {
+        textInVoice.value = "";
+      }
+      textInVoice.focus();
+    }
+    this.recognition.onresult = function (event) {
+      textInVoice.blur();
+      var texto = "";
+      for (var i = event.resultIndex; event.results.length > i; ++i) {
+        if (event.results[i].isFinal) {
+          texto += event.results[i][0].transcript;
+        }
+      }
+      textInVoice.value = texto;
+    }
+  }
+
+  pararAudicaoResposta() {
+    this.recognition.stop();
+    this.respostaVazia = false;
   }
 }
